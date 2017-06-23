314f372a83c9e82c507787d96f2090f04b9ead46
ch1bi <maine1191@gmail.com>
2017-06-23 18:16:57 -0400

    added score to game

--- js/controllers/cardController.js
+++ js/controllers/cardController.js
@@ -1,17 +1,37 @@
 angular.module("cardApp", [])
 
-.controller("cardControl", ["$scope", "$http", "$timeout", function($scope, $http, $timeout){
+.controller("cardControl", ["$scope", "$http", "$timeout", function($scope, $http){
+    
+    $scope.score = 0
+
+    var scores = {
+
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        "QUEEN": 10,
+        "JACK": 10,
+        "KING": 10,
+        "ACE": 11
+
+    };
+
 
 $http.get("https://deckofcardsapi.com/api/deck/0v6urxy4dxu3/shuffle/")
 
 
 .then(function(response){
 
-
+//when page loads display message
 $scope.message = true;
 
-
-  
 })
 
 $scope.deal = function(){
@@ -20,10 +40,24 @@ $scope.deal = function(){
 $http.get("https://deckofcardsapi.com/api/deck/0v6urxy4dxu3/draw/?count=1")
 
 .then(function(data){
-
+    
+    
+    //when card is dealt, hide message
     console.log(data);
     $scope.message = false;
     $scope.image = data.data.cards[0].image
+    
+    var cardValue = data.data.cards[0].value;
+
+   $scope.score += scores[cardValue]
+
+
+
+    //get card value
+
+    //add value to current score
+
+    //check if went over 21
 })
 
 }
